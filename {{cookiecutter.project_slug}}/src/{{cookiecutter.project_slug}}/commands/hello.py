889a601ebc78458fffb14372827310114bc2f240
Cecron <b9kvz7be6v@snkmail.com>
2024-06-27 07:59:34 +0200

    Add description text to subcommand hello.

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/commands/hello.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/commands/hello.py
@@ -6,7 +6,8 @@ class Hello:
         self.parser = subparsers.add_parser('hello', help="Great someone by name.")
         self.parser.add_argument(
             '-n', '--name', metavar="NAME", required=False, type=str, dest='name', default="World",
-            help="Name to greet. (default: %(default)s)"
+            help="Name to greet. (default: %(default)s)",
+            description="Name to greet. (default: %(default)s)",
         )
         self.parser.set_defaults(action=self)
 
